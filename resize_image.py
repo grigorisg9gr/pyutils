8a1dee25adf5368cecba1ccf349fbdc214b3ccb9
Grigoris <grigoris.chrysos@gmail.com>
2015-03-16 18:12:42 +0000

    Change the image_resize.py so as to change all images in a folder in the same size. This is a step required if we want to convert them to video later.

--- resize_image.py
+++ resize_image.py
@@ -5,36 +5,51 @@
 # images it finds in its path.
 
 import sys
+from PIL import Image
+import os
 
-def resize(folder, fileName, factor=None):
-    from PIL import Image
-    import sys, os
+def load_image(folder, fileName, imgExts):
+    ext = fileName[-3:].lower()
+    if ext not in imgExts:
+        return
     filePath = os.path.join(folder, fileName)
     im = Image.open(filePath)
-    w, h  = im.size
-    if factor == None:
-        newIm = im.resize((int((w/2)*2), int((h/2)*2)))
+    w, h = im.size
+    return im, w, h
+
+def resize(folder, fileName, factor=None, imgExts=["png", "bmp", "jpg"], w_base=2, h_base=2):
+    im, w, h = load_image(folder, fileName, imgExts)
+    filePath = os.path.join(folder, fileName)
+    if factor is None:
+        # ensure that an image of dimension 640x320 and one of 639x319 will be at the same size after resizing.
+        newIm = im.resize((w_base, h_base))
     else:
         newIm = im.resize((int(w*factor), int(h*factor)))
     newIm.save(filePath)
 
 
 def bulkResize(imageFolder, factor=None):
-    import sys, os
     imgExts = ["png", "bmp", "jpg"]
     for path, dirs, files in os.walk(imageFolder):
-        for fileName in files:
-            ext = fileName[-3:].lower()
-            if ext not in imgExts:
-                continue
+        if len(files) == 0:
+            continue
+        _, w, h = load_image(path, files[0], imgExts)  # load the first image, in order to define the shape (h, w)
+        w = int((w/2)*2)
+        h = int((h/2)*2)
+        try:
+            from joblib import Parallel, delayed
+            Parallel(n_jobs=-1, verbose=4)(delayed(resize)(path, fileName, factor, imgExts, w, h) for fileName in files)
+        except:
+            print('Sequential execution')
+            for fileName in files:
+                resize(path, fileName, factor, imgExts, w, h)
 
-            resize(path, fileName, factor)
 
 
 if __name__ == "__main__":
     imageFolder = sys.argv[1] # first arg is path to image folder
     if len(sys.argv) > 2:
-        resizeFactor=float(sys.argv[2])/100.0# 2nd is resize in %
+        resizeFactor = float(sys.argv[2])/100.0 # 2nd is resize in %
     else:
         resizeFactor = None
     bulkResize(imageFolder, resizeFactor)
--- video2frames.py
+++ video2frames.py
@@ -34,6 +34,7 @@ def clip_to_frames(clip_name, path_video, path_fr_0):
     Accepts a clip file and converts it into individual frames by calling avconv. 
     It might rename the original file if it contains whitespaces (just removes thems).
     """
+    print clip_name
     if clip_name[-3:]!= type_v:
         print('Ignoring file ' + clip_name); return  
     #_name = clip_name; name = _name.replace(' ','')           # replace all empty spaces in the original file
