b36d17914ea22a2b8781842ee76142faa1094a8c
Grigoris <grigoris.chrysos@gmail.com>
2016-08-22 19:19:06 +0200

    Expose the methods to the top level, i.e. to be able to be imported right from the pyutils level.

--- research_pyutils/__init__.py
+++ research_pyutils/__init__.py
@@ -1,1 +1,16 @@
-__author__ = 'grigoris'
+# expose the most frequently used functions in the top level.
+from .path_related_functions import (mkdir_p, rm_if_exists,
+                                     remove_empty_paths,
+                                     copy_contents_of_folder,
+                                     copy_the_previous_if_missing)
+
+from .menpo_related import (resize_all_images, from_ln_to_bb_path,
+                            process_lns_path)
+
+from .filenames_changes import (rename_files, change_suffix,
+                                strip_filenames)
+
+from .auxiliary import (execution_stats, compare_python_types,
+                        whoami)
+
+
