ef5f386a85ac92a74392bcd4d89f51a977dd0599
Grigoris <grigoris.chrysos@gmail.com>
2017-01-31 09:37:37 +0000

    Optional import of menpo modules, e.g. in case menpo is not installed.

--- research_pyutils/__init__.py
+++ research_pyutils/__init__.py
@@ -3,9 +3,15 @@ from .path_related import (mkdir_p, rm_if_exists, remove_empty_paths,
                            copy_contents_of_folder, count_files, 
                            copy_the_previous_if_missing)
 
-from .menpo_related import (resize_all_images, from_ln_to_bb_path,
-                            process_lns_path, compute_overlap,
-                            rasterize_all_lns)
+try:
+    from .menpo_related import (resize_all_images, from_ln_to_bb_path,
+                                process_lns_path, compute_overlap,
+                                rasterize_all_lns)
+except ImportError:
+    m1 = ('The menpo related utils are not imported. If '
+          'you intended to use them please check your '
+          'menpo installation.')
+    print(m1)
 
 from .filenames_changes import (rename_files, change_suffix,
                                 strip_filenames)
